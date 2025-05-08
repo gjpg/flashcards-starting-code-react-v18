5d3c220693e7507a81941177c722b6b430806d25
George <george.j.p.gibb@googlemail.com>
2025-05-08 12:29:03 +0100

    topics page

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import reducers
+import topicsSlice from "../features/topics/topicsSlice";
 
 export default configureStore({
   reducer: {
-    
+    topics: topicsSlice,
   },
 });
